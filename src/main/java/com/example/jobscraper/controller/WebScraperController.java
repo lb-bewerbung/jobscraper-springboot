c4dabd24be7fa7abc9380f79e4b4470e7ba2a3f7
lb-bewerbung <bewerbung@lb.net>
2024-11-08 21:23:30 +0100

    Added new route to extract links and jobtitles from stepstone search page

--- src/main/java/com/example/jobscraper/controller/WebScraperController.java
+++ src/main/java/com/example/jobscraper/controller/WebScraperController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class WebScraperController {
 
@@ -20,5 +23,10 @@ public class WebScraperController {
     @GetMapping("/scrapestepstonesp")
     public String scrapeStepstoneSearchPage(@RequestParam String url) {
         return webScraperService.scrapeStepstoneSearchPage(url);
+    }
+
+    @GetMapping("/extractjoblinks")
+    public List<Map<String, String>> extractJoblinksAndJobtitles(@RequestParam String url) {
+        return webScraperService.extractJoblinksAndJobtitles(url);
     }
 }
