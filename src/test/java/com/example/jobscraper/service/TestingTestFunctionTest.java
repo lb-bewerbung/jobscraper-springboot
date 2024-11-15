faaa29e05a5ff3db3236b4f681a7e5723dd508e7
lb-bewerbung <bewerbung@lb.net>
2024-11-15 16:07:06 +0100

    Added dummy test that is always green for jenkins test purpose

---
+++ src/test/java/com/example/jobscraper/service/TestingTestFunctionTest.java
@@ -0,0 +1,13 @@
+package com.example.jobscraper.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestingTestFunctionTest {
+
+    @Test
+    public void testAlwaysGreen() {
+        // Ein Dummy-Test, der immer "grün" ist
+        assertTrue(true);
+    }
+}
