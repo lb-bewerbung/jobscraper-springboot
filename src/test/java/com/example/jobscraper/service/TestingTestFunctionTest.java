9b2202f3af19c9f3e2cd43f9664afc8f0d5df32f
lb-bewerbung <bewerbung@lb.net>
2024-11-15 16:28:45 +0100

    Github webhook test

--- src/test/java/com/example/jobscraper/service/TestingTestFunctionTest.java
+++ src/test/java/com/example/jobscraper/service/TestingTestFunctionTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestingTestFunctionTest {
 
+
     @Test
     public void testAlwaysGreen() {
         // Ein Dummy-Test, der immer "grün" ist
